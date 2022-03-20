c7afa9054d482c8d57d34480ec9fb4a3413b3265
Sourav <s6hr6h4u3@gmail.com>
2022-03-20 20:27:34 +0530

    initial commit

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,78 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+    const [shadeDifference, setShadeDifference] = useState(10);
+    const [InputColor, setInputColor] = useState("#f15025");
+    const [error, setError] = useState(false);
+    const [List, setList] = useState(new Values("#f15025").all(10));
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            let colors = new Values(InputColor).all(shadeDifference);
+            setError(false);
+
+            setList(colors);
+        } catch (error) {
+            setError(true);
+        }
+    };
+
+    return (
+        <>
+            <section className="container">
+                <h3>color generator</h3>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="color-val">Hex color</label>
+                        <input
+                            className={`${error === true ? "error" : ""}`}
+                            type="text"
+                            id="color-val"
+                            value={InputColor}
+                            placeholder="Enter Color Code here"
+                            onChange={(e) => {
+                                setInputColor(e.target.value);
+                            }}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="shade-val">Shade Difference</label>
+                        <input
+                            type="number"
+                            id="shade-val"
+                            placeholder="Shade Difference"
+                            value={shadeDifference}
+                            onChange={(e) => {
+                                setShadeDifference(parseInt(e.target.value));
+                            }}
+                        />
+                    </div>
+
+                    <button type="submit" className="btn">
+                        submit
+                    </button>
+                </form>
+            </section>
+            <section className="colors">
+                {List.map((color, index) => {
+                    return (
+                        <SingleColor
+                            key={index}
+                            rgb={color.rgb}
+                            weight={color.weight}
+                            type={color.type}
+                            hex={color.hex}
+                        />
+                    );
+                })}
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ weight, type, hex }) => {
+    const [alert, setAlert] = useState("");
+    const hexVal = `#${hex}`;
+    const handleColorClick = (val) => {
+        setAlert(val);
+        navigator.clipboard.writeText(val);
+        setTimeout(() => {
+            setAlert("");
+        }, 2500);
+    };
+    return (
+        <>
+            <article
+                onClick={() => handleColorClick("#" + hex)}
+                className={`color ${type === "shade" ? "color-light" : ""}`}
+                style={{ background: hexVal }}>
+                <p className="percent-value">{weight}%</p>
+                <p className="color-value">{hexVal}</p>
+                {alert === hexVal ? <p className="alert">copied</p> : <></>}
+            </article>
+        </>
+    );
+};
 
-export default SingleColor
+export default SingleColor;
